4d77a0438d746dc6d8f8fcee471de07bd21f2b77
KafuiBee <168825021+KafuiBee@users.noreply.github.com>
2024-05-31 13:02:49 +0100

    added axios and api key

--- Src/index.js
+++ Src/index.js
@@ -1,3 +1,10 @@
+function searchCity(city) {
+  let apiKey = "4108a6e3f140d4b07bc38ef3f66c2t5";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
