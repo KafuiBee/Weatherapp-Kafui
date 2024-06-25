13549641c88b6a64231b76ee34d81ef18e6e511e
KafuiBee <168825021+KafuiBee@users.noreply.github.com>
2024-06-25 11:43:11 +0100

    logged API response

--- Src/index.js
+++ Src/index.js
@@ -1,8 +1,7 @@
 function searchCity(city) {
-  let apiKey = "4108a6e3f140d4b07bc38ef3f66c2t5";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
-  console.log(apiUrl);
+  let apiKey = "4108a6e3f140d4b07bc38ef3f66c2t5o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
 }
 
 function handleSearchSubmit(event) {
@@ -11,7 +10,8 @@ function handleSearchSubmit(event) {
   let SearchFormElement = document.querySelector("#search-form");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchCityInput.value);
 }
 
-let SearchFormElement = document.querySelector("#search-form");
-SearchFormElement.addEventListener("submit", handleSearchSubmit);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
